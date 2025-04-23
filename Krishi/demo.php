a51eafaed87555823a3c52730ac90d6443550152
EsHiTa-Y-2025 <eshitay.agent@gmail.com>
2025-04-24 00:12:23 +0530

    Delete Krishi directory

--- Krishi/demo.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$num = 12345;
-echo md5($num);
-?>
--- Krishi/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['admin_url'])){
-	unset($_SESSION['admin_url']);
-}
-header('location:login');
-die;
-?>
