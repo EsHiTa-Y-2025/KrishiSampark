d16b2a46c39224b15c3fe3b438a9d1ab86475c86
EsHiTa-Y-2025 <eshitay.agent@gmail.com>
2025-04-24 10:55:22 +0530

    Add files via upload

---
+++ Krishi/private/footer.php
@@ -0,0 +1,161 @@
+  <!-- Start Footer Section -->
+        <section class="footer-section footer-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>About Krishisampark</h3>
+                            </div>
+
+                            <p>Krishisampark is an agricultural platform that offers a comprehensive range of services to farmers, including those related to crop production, livestock management, and animal trade. Here is some content highlighting the services provided by Krishisampark:
+                            </p>
+
+                            <ul class="footer-map">
+                                <li>
+                                    <a href="" target="_blank">
+                                        <i class="flaticon-right"></i>
+                                        FIND US ON MAP
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>Our Support</h3>
+                            </div>
+
+                            <ul class="footer-quick-links">
+                                <li>
+                                    Help and Ordering
+                                </li>
+                                <li>
+                                    Return & Cancellation
+                                </li>
+                                <li>
+                                    Delevery Schedule
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>Contact info</h3>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-pin"></i>
+                                <h3>Location</h3>
+                                <span>Hubbali <br>Karnataka, India</span>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-call"></i>
+                                <h3>Call Us</h3>
+                                <span><a href="tel:+919481725226">+919481725226</a></span>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-envelope"></i>
+                                <h3>Email Us</h3>
+                                <span><a>info@krishisampark.in</a></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>Contact Us</h3>
+                            </div>
+                           
+
+                            <form class="newsletter-form">
+                                <input type="text" class="input-newsletter mb-2" placeholder="Enter your name" name="name" required autocomplete="off">
+                                <input type="number" class="input-newsletter mb-2" placeholder="Enter your mobile number" name="phone" required autocomplete="off">
+                                <input type="email" class="input-newsletter mb-2" placeholder="Enter your email" name="EMAIL" required autocomplete="off">
+                                <button type="submit">
+                                    Contact
+                                    
+                                </button>
+                                <div id="validator-newsletter" class="form-result"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="copyright-area">
+                <div class="container">
+                    <div class="copyright-area-content">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6">
+                                <p>
+                                    Copyright @<script>document.write(new Date().getFullYear())</script> Krishisampark. All Rights Reserved by
+                                    <a href="https://crawlerstechnologies.com/" target="_blank">
+                                        Krishisampark
+                                    </a>
+                                </p>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6">
+                                <ul>
+                                    <li>
+                                        <a href="terms-and-conditions.php">Terms & Conditions</a>
+                                    </li>
+                                    <li>
+                                        <a href="privacy-policy.php">Privacy Policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Footer Section -->
+
+        <!-- Start Go Top Section -->
+        <div class="go-top">
+            <i class="fas fa-chevron-up"></i>
+            <i class="fas fa-chevron-up"></i>
+        </div>
+        <!-- End Go Top Section -->
+
+        <!-- jQuery Min JS -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap Min JS -->
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <!-- Owl Carousel Min JS -->
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <!-- MeanMenu JS -->
+        <script src="assets/js/jquery.meanmenu.js"></script>
+        <!-- Popup JS -->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- Nice Selects Min JS -->
+        <script src="assets/js/jquery.nice-select.min.js"></script>
+        <!-- Appear JS -->
+        <script src="assets/js/jquery.appear.min.js"></script>
+        <!-- Odometer JS -->
+        <script src="assets/js/odometer.min.js"></script>
+        <!-- Image LightBox Min JS -->
+        <script src="assets/js/imagelightbox.min.js"></script>
+        <!-- AjaxChimp Min JS -->
+        <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+        <!-- Form Validator Min JS -->
+        <script src="assets/js/form-validator.min.js"></script>
+        <!-- Contact Form Min JS -->
+        <script src="assets/js/contact-form-script.js"></script>
+        <!-- Wow Min JS -->
+        <script src="assets/js/wow.min.js"></script>
+        <!-- Main JS -->
+        <script src="assets/js/main.js"></script>
+    </body>
+
+</html>
---
+++ Krishi/private/header.php
@@ -0,0 +1,100 @@
+<?php require_once('private/autoload.php');?>
+<!doctype html>
+<html lang="en">
+    
+<head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap Min CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <!-- Animate Min CSS -->
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <!-- FontAwesome Min CSS -->
+        <link rel="stylesheet" href="assets/css/all.min.css">
+        <!-- FlatIcon CSS -->
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <!-- Popup CSS -->
+        <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+        <!-- Owl Carousel Min CSS -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <!-- Image LightBox Min CSS -->
+        <link rel="stylesheet" href="assets/css/imagelightbox.min.css">
+        <!-- Nice Select Min CSS -->
+        <link rel="stylesheet" href="assets/css/nice-select.min.css">
+        <!-- MeanMenu CSS -->
+        <link rel="stylesheet" href="assets/css/meanmenu.css">
+        <!-- Odometer CSS -->
+        <link rel="stylesheet" href="assets/css/odometer.min.css">
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- Dark CSS -->
+        <link rel="stylesheet" href="assets/css/dark.css">
+        <!-- Responsive CSS -->
+        <link rel="stylesheet" href="assets/css/responsive.css">
+
+        <title>Krishisampark - Agriculture Products & Services | Animal Trade</title>
+
+        <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    </head>
+
+    <body>
+
+        <!-- Start Preloader Area -->
+		<!--<div class="preloader">-->
+		<!--	<div class="lds-ripple">-->
+		<!--		<div></div>-->
+		<!--		<div></div>-->
+		<!--	</div>-->
+		<!--</div>-->
+        <!-- End Preloader Area --> 
+
+        <!-- Start Navbar Area -->
+        <div class="navbar-area navbar-style-one">
+            <div class="trifles-responsive-nav">
+                <div class="container">
+                    <div class="trifles-responsive-menu">
+                        <div class="logo">
+                            <a href="">
+                                <img src="<?=$baseurl;?>app/img/krishi.png" style='width:100px' alt="logo"> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="trifles-nav">
+                <div class="container">
+                    <nav class="navbar navbar-expand-md navbar-light">
+                        <a class="navbar-brand" href="">
+                             <img src="<?=$baseurl;?>app/img/krishi.png" style='width:100px' alt="logo"> 
+                            <!--<h4 class="" style="color: rgb(6, 187, 6);font-family:cursive;">Krishisampark</h4>-->
+                        </a>
+
+                        <div class="collapse navbar-collapse mean-menu" >
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link ">
+                                        Home 
+                                    </a>
+                                </li>
+
+                              
+
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        Contact
+                                    </a>
+                                </li>
+                            </ul>
+
+                           
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- End Navbar Area -->
+
+       
